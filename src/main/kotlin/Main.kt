e7cfc1c858369adc13fec6308a967c275ed9b906
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-07 13:48:25 +0000

    Añadido imagenes y puestas imagenes inicales historias

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
@@ -21,16 +22,27 @@ fun App(){
     ){
         Text(
             text = "Instagram",
-            fontSize = 24.sp
+            fontSize = 36.sp
         )
         Divider(
             modifier = Modifier.fillMaxWidth(),
             color = Color.Gray,
             thickness = 1.dp
         )
-        Text(text = "Historias")
+        Text(
+            text = "Historias",
+            fontSize = 24.sp
+        )
+        Row {
+            images.forEach { image ->
+                Image(
+                    painter = painterResource(resourcePath = image.image),
+                    contentDescription = "Foto Perfil",
+                    modifier = Modifier.size(70.dp).padding(10.dp).clip(CircleShape)
+                )
+            }
+        }
     }
-
 }
 
 fun main() = application {
@@ -38,7 +50,7 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "Fake Instagram",
         state = rememberWindowState(width = 1000.dp, height = 600.dp)
-    ){
+    ) {
         App()
     }
 }
--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -1,19 +1,28 @@
+import androidx.compose.foundation.Image
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 
-data class Message(val name: String, val message: String, val image: String)
+data class Message(val name: String, val message: String)
 val body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 val message = listOf<Message>(
-    Message("Persona1 Apellido1", body, "img1.jpeg"),
-    Message("Persona2 Apellido2", body, "img2.jpeg"),
-    Message("Persona3 Apellido3", body, "img3.jpeg")
+    Message("Persona1 Apellido1", body),
+    Message("Persona2 Apellido2", body),
+    Message("Persona3 Apellido3", body)
 )
 
 val items = listOf(
     "Home" to Icons.Default.Home,
     "Search" to Icons.Default.Search,
     "Setting" to Icons.Default.Settings
+)
+
+data class Image(val image: String)
+
+val images = listOf(
+    Image("img1.jpeg"),
+    Image("img2.jpeg"),
+    Image("img3.jpeg")
 )
