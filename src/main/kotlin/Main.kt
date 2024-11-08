f1de580d3d5a881d7bf6739832f0d3232c74526c
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-08 13:13:47 +0000

    Añadido imagenes y avanzado en historias y en publicacion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,21 @@
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,7 +26,8 @@ import androidx.compose.ui.window.rememberWindowState
 @Composable
 fun App(){
     Column (
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.background(Color(0xfff6f6f6)).fillMaxSize()
     ){
         Column(
             modifier = Modifier.background(Color.White).fillMaxWidth().height(70.dp),
@@ -27,30 +35,103 @@ fun App(){
         ){
             Text(
                 text = "Instagram",
-                fontSize = 36.sp
+                fontSize = 36.sp,
+                modifier = Modifier.padding(start = 16.dp)
             )
         }
-        Column(
-            modifier = Modifier.background(Color.Gray).fillMaxWidth()
-        ) {
-            Text(
-                text = "Historias",
-                fontSize = 24.sp
-            )
-            Card(
-                modifier = Modifier.padding(top = 15.dp),
-                backgroundColor = Color(0xffc6b1c9),
-                elevation = 10.dp
-            ){
+        Row {
+            Column(
+                modifier = Modifier
+            ) {
+                Text(
+                    text = "Historias",
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
                 Row {
-                    images.forEach { image ->
-                        Image(
-                            painter = painterResource(resourcePath = image.image),
-                            contentDescription = "Foto Perfil",
-                            modifier = Modifier.size(90.dp).padding(10.dp).clip(CircleShape)
-                        )
+                    Card(
+                        modifier = Modifier.padding(16.dp).width(520.dp),
+                        backgroundColor = Color.White,
+                        elevation = 2.dp,
+                        border = BorderStroke(2.dp,Color(0xffc5c5c5))
+                    ){
+                        Row {
+                            images.forEach { image ->
+                                Image(
+                                    painter = painterResource(resourcePath = image.image),
+                                    contentDescription = "Foto Perfil",
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                        .size(70.dp)
+                                        .clip(CircleShape)
+                                        .border(
+                                            width = 2.dp,
+                                            brush = Brush.linearGradient(
+                                                colors = listOf(Color.Yellow, Color.Red),
+                                                start = Offset(0f, 0f),
+                                                end = Offset(70f, 70f)
+                                            ),
+                                            shape = CircleShape
+                                        )
+                                )
+                            }
+                        }
                     }
                 }
+                Text(
+                    text = "Publicaciones",
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+                publications.forEachIndexed { index, publication ->
+                    Card(
+                        modifier = Modifier.padding(16.dp).width(520.dp).height(350.dp),
+                        backgroundColor = Color.White,
+                        elevation = 2.dp,
+                        border = BorderStroke(2.dp,Color(0xffc5c5c5))
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Column(
+                                modifier = Modifier.height(290.dp)
+                            ) {
+                                Image(
+                                    painter = painterResource(resourcePath = publication.image),
+                                    contentDescription = "Foto Publicacion",
+                                    contentScale = ContentScale.Crop
+                                )
+                            }
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.White)
+                                    .height(60.dp)
+                                    .border(width = 1.dp, color = Color(0xffc5c5c5))
+                            ) {
+                                Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Image(
+                                        painter = painterResource(resourcePath = images[index].image),
+                                        contentDescription = "Foto Perfil",
+                                        modifier = Modifier
+                                            .padding(8.dp)
+                                            .size(40.dp)
+                                            .clip(CircleShape)
+                                    )
+                                    Text(
+                                        text = name[index].name,
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Column {
+
             }
         }
     }
@@ -60,7 +141,7 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Fake Instagram",
-        state = rememberWindowState(width = 1000.dp, height = 600.dp)
+        state = rememberWindowState(width = 1000.dp, height = 1100.dp)
     ) {
         App()
     }
--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -3,14 +3,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import javax.naming.Name
 
 data class Message(val name: String, val message: String)
 val body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 val message = listOf<Message>(
-    Message("Persona1 Apellido1", body),
-    Message("Persona2 Apellido2", body),
-    Message("Persona3 Apellido3", body)
+    Message("Persona1", body),
+    Message("Persona2", body),
+    Message("Persona3", body)
 )
 
 val items = listOf(
@@ -20,9 +21,31 @@ val items = listOf(
 )
 
 data class Image(val image: String)
+data class Name(val name: String)
+
+val name = listOf(
+    Name("Diana Amoros"),
+    Name("Alex Ares"),
+    Name("Aaron Liu"),
+    Name("Maria Cruz"),
+    Name("Adelina Lucas"),
+    Name("Jose Enrique"),
+    Name("Laura Valera"),
+    Name("Javier Paz"),
+    Name("Gloria Bermejo"),
+    Name("Leopoldo Vila")
+)
 
 val images = listOf(
     Image("img1.jpeg"),
     Image("img2.jpeg"),
-    Image("img3.jpeg")
+    Image("img3.jpeg"),
+    Image("img4.jpeg"),
+    Image("img5.jpeg"),
+    Image("img6.jpeg")
+)
+
+val publications = listOf(
+    Image("img12.jpg"),
+    Image("img13.jpg")
 )
